359852aacd8e5fbb79c60e1e4cbfefe82fbdc98c
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-06-15 13:15:50 +0700

    Update to 0.0.1l

--- ORM/TopmenuORM.go
+++ ORM/TopmenuORM.go
@@ -8,6 +8,7 @@ import (
 type TopMenu struct {
 	Id      uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	Caption string    `gorm:"caption"`
+	Type    string
 	Items   []SubMenu `gorm:"foreignkey:TopMenuId"`
 }
 type TopMenuORM struct {
