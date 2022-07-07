46dec9dab856d719be8c28bcdfa8faff068a3459
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-07 14:28:39 +0700

    Update to 0.0.4g

--- ORM/RoleORM.go
+++ ORM/RoleORM.go
@@ -3,7 +3,6 @@ package ORM
 import "github.com/gofrs/uuid"
 
 type Role struct {
-	Id          uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Rolename    string    `gorm:"not null;rolename"`
-	Description string    `gorm:"not null;description"`
+	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Rolename string    `gorm:"not null;rolename"`
 }
