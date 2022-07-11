b93b91f288d59002c47138ea44a53efe714e50c1
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-11 09:21:51 +0700

    Update to 0.0.4h

--- ORM/UsersORM.go
+++ ORM/UsersORM.go
@@ -1,18 +1,22 @@
 package ORM
 
 import (
+	"time"
+
 	"github.com/MrAmperage/GoWebStruct/WebCore/Modules/ORMModule"
 	"github.com/gofrs/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Username string    `gorm:"primaryKey;not null;username"`
-	Password string    `gorm:"not null;password"`
-	Enabled  bool      `gorm:"not null;enabled"`
-	RoleId   string    `gorm:"not null;rolename"`
-	Role     Role      `gorm:"foreignkey:Id;references:RoleId"`
+	Id        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Username  string    `gorm:"primaryKey;not null;username"`
+	Password  string    `gorm:"not null;password"`
+	Enabled   bool      `gorm:"not null;enabled"`
+	RoleId    string    `gorm:"not null;rolename"`
+	Role      Role      `gorm:"foreignkey:Id;references:RoleId"`
+	StartDate time.Time `gorm:"not null;start_date"`
+	EndDate   time.Time `gorm:"not null;end_date"`
 }
 
 type UserORM struct {
