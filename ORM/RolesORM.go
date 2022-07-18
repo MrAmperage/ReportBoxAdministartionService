00f08732032c6fc56506a1cf9d91f5c06b937f1b
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-18 09:42:38 +0700

    Update to 0.0.4o

--- ORM/RolesORM.go
+++ ORM/RolesORM.go
@@ -3,11 +3,14 @@ package ORM
 import (
 	"github.com/MrAmperage/GoWebStruct/WebCore/Modules/ORMModule"
 	"github.com/gofrs/uuid"
+	"github.com/lib/pq"
 )
 
 type Role struct {
-	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Rolename string    `gorm:"not null;rolename"`
+	Id           uuid.UUID      `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Rolename     string         `gorm:"not null;rolename"`
+	MenusAccess  pq.StringArray `gorm:"type:varchar[];default:array[]::varchar[];menus_access"`
+	GroupsAccess pq.StringArray `gorm:"type:varchar[];default:array[]::varchar[];groups_access"`
 }
 type RolesORM struct {
 	ORMModule.ORM
